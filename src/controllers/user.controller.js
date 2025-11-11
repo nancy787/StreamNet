5457d944831c2da6e5c3afec66b2b52df5379e43
nancy787 <nancykumari2630@gmail.com>
2025-11-11 22:15:40 +0530

    Added register api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,69 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { APIError } from "../utils/APIError.js";
+import {User} from "../models/user.model.js";
+import {uploadOnCloundinary} from "../utils/cloudinary.js";
+import { APIResponse } from "../utils/APIResponse.js";
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json( {
-        message : "user register successfully"
-    })
-})
+   const {FullName, email, username, password} = req.body
+   console.log(req.body);
+   
+   if( [ FullName, email, username, password ].some( (field)  =>  
+      field?.trim() === " ") 
+      ) 
+      {
+         throw new APIError(400, 'All fileds are required')
+      }
 
-export {registerUser}
+      const existedUser =await User.findOne( {
+         $or : [ {username} , {email}]
+      });
+
+      if(existedUser) {
+         throw new APIError(400, 'username or email already exists')
+      }
+
+      const avatarLocalPath = req.files?.avatar[0]?.path;
+      const coverImagelocalPath = req.files?.coverImage[0]?.path;
+
+      if(!avatarLocalPath) {
+         throw new APIError(400, 'avatar image is not found')
+      }
+      console.log("req");
+
+
+      const avatarImg    =  await uploadOnCloundinary(avatarLocalPath)
+      const coverImg = await uploadOnCloundinary(coverImagelocalPath)
+      
+      if(!avatarImg) {
+         throw new APIError(400, 'image not added on serev')
+      }
+
+      const user = await User.create( {
+         FullName, 
+         avatar : avatarImg.url,
+         coverImage : coverImg?.url || '',
+         email,
+         password,
+         username : username.toLowerCase
+         })
+
+         console.log(user);
+         const getUser = await User.findById(user.__id).select(
+            "-password -refreshToken", 
+         )
+
+         console.log(getUser);
+
+         if(!getUser) {
+            throw new APIError(500, 'someting went wrg while registering the user')
+         }
+         
+         return res.status(201).json(
+            new APIResponse(200, getUser, 'user registerd successfully')
+         )
+
+});
+
+export {registerUser,}
+
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
-import { use } from "react";
 
 const userSchema  = new Schema(
 {
@@ -55,7 +54,7 @@ const userSchema  = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return  next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 //to define custom method
